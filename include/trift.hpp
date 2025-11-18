078ae0d2dcc0ad3300ecb3b3e77a91bb4d043e5d
Naila <zahwanaila27@gmail.com>
2025-11-18 09:07:20 -0800

    Add comments for clothing-related procedures and functions

--- include/trift.hpp
+++ include/trift.hpp
@@ -1,1 +1,7 @@
-
+// procedure create tree pakaian
+// function create node pakaian
+// procedure insert data pakaian (kategori: kaos/kemeja/rok/celana; harga: mahal(x > 50.000)/murah( x <= 50.000); model: bermotif/polos/bergaris
+// function menghitung banyak pakaian yang yang memiliki harga mahal untuk kategori tertentu
+// function menampilkan data pakaian yang termahal dari setiap kategori
+// procedure display data tree
+// 

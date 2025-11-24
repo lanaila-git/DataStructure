8dcbbacb1ece97ab2be32953b419b269b4f8443a
Naila <zahwanaila27@gmail.com>
2025-11-24 04:58:31 -0800

    Update trift.hpp

--- include/trift.hpp
+++ include/trift.hpp
@@ -1,5 +1,6 @@
-// procedure create tree pakaian
-// function create node pakaian
+// procedure create tree lemari
+// function create node item pakaian
+// procedure insert item berdasarkan kode unik (< 40 atasan; > 40 bawahan)
 // procedure insert data pakaian (kategori: kaos/kemeja/rok/celana; harga: mahal(x > 50.000)/murah( x <= 50.000); model: bermotif/polos/bergaris
 // function menghitung banyak pakaian yang yang memiliki harga mahal untuk kategori tertentu
 // function menampilkan data pakaian yang termahal dari setiap kategori
